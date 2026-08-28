03cc16ee6acbbe4262f65a90b6f380e49827b66e
joshuecm <joshuecm@gmail.com>
2026-08-28 12:30:11 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+      }
+    }
+  }
 })
